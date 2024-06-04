f2f32cc3a9f0a97b75f5997dfdadf7e4a4155cd1
Hancie123 <hanciewanemphago@gmail.com>
2024-06-04 23:19:34 +0545

    withdraw history added

--- resources/views/withdraw_history.blade.php
+++ resources/views/withdraw_history.blade.php
@@ -59,7 +59,7 @@
 
 
                 @empty
-                    <img class="mx-auto d-block" src="{{ url('assets/img/no-data.png') }}" style="width: 80px;" />
+                    <img class="mx-auto d-block" src="{{ url('assets/img/no-data.png') }}" style="width: 100px;" />
                     <h5 class="text-center p-2">You have no withdrawal history!</h5>
                 @endforelse
             </ul><br>
