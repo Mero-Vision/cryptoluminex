7d86a3c882b5c9bd646030f103bf84bb1d601763
Hancie123 <hanciewanemphago@gmail.com>
2024-06-06 23:34:59 +0545

    mail design added

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td>
+<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td class="content-cell" align="center">
+Crypto Luminex © 2024
+</td>
+</tr>
+</table>
+</td>
+</tr>
