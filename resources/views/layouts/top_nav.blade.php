e4dfa0012d3dc540d6d8b40b624217e3da135b6c
Hancie123 <hanciewanemphago@gmail.com>
2024-06-10 14:16:57 +0545

    nav updated

--- resources/views/layouts/top_nav.blade.php
+++ resources/views/layouts/top_nav.blade.php
@@ -3,11 +3,11 @@
                 <a href="{{url('user-info')}}">
                     <img src="{{ url('assets/img/logo.png') }}" alt="img" class="avt">
                 </a>
-                <div class="search-box box-input-field style-2">
+                {{-- <div class="search-box box-input-field style-2">
                     <a href="home-search.html" class="icon-search"></a>
                     <input type="text" placeholder="Looking for crypto" required class="clear-ip">
                     <i class="icon-close"></i>
-                </div>
+                </div> --}}
             </div>
             <div class="d-flex align-items-center gap-8">
                 <a href="{{url('logout')}}"><i class="bi bi-box-arrow-in-right bx-sm"></i></a>
